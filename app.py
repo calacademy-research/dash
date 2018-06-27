60b4e38f430ab577691b074a104da6cd4a75a43c
Joe Russack <joe@nothingugly.com>
2018-06-26 17:52:42 -0700

    ugly working mess

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
 import sys
+import socketio
+import subprocess
+from flask_socketio import SocketIO
+import string
+
+
 
 from flask import Flask
 from flask import render_template, request
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../genomics-ansible"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "genomics-ansible"))
 import yaml_backend
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 
 @app.route('/')
@@ -18,7 +26,7 @@ def index():
 
 @app.route('/available-packages', methods=['GET', 'POST'])
 def available_packages():
-    packages = yaml_backend.load_packages("/Users/dash/Documents/dash/genomics-ansible/role_definitions")
+    packages = yaml_backend.load_packages(os.path.join(os.path.dirname(__file__), "genomics-ansible/role_definitions"))
 
     if request.method == 'POST':
         selected_packages = request.form.getlist('package_checkbox')
@@ -30,5 +38,65 @@ def available_packages():
     return render_template('package_list.html', packages=packages)
 
 
+def send_server(run_string):
+    global socketio
+    print(run_string)
+    printable = set(string.printable)
+    no_errors = filter(lambda x: x in printable, run_string)
+    if socketio is not None:
+        socketio.emit('run_log', no_errors)
+    print("Emmtted:" + no_errors)
+
+def run_process(arg_array, directory=None,  server_echo=False):
+    global socketio
+
+
+    bin = arg_array[0]
+    if socketio is not None:
+        send_server('running ' + bin + "\n")
+    # print("Executing script:" + arg_array)
+    # pass universal_newlines=True so Carriage Return interpreted as newline
+    try:
+        if directory is None:
+            p = subprocess.Popen(arg_array, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        else:
+            p = subprocess.Popen(arg_array, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=directory, universal_newlines=True)
+
+        results = ""
+        stdout_lines = iter(p.stdout.readline, "")
+        for line in stdout_lines:
+            if server_echo:
+                send_server(line)
+            results += line
+        # retcode = p.poll()
+    except Exception as e:
+        send_server("Run aborted due to error:  " + e.strerror)
+        send_server("Current working directory: " + directory)
+        send_server("Command:                   " + bin)
+        if socketio is not None:
+            socketio.emit('done', bin)
+        raise Exception(e.strerror)
+    if socketio is not None:
+        socketio.emit('done', bin)
+    return results
+
+
+
+@app.route('/ls-template', methods=['GET'])
+def ls_template():
+
+    return render_template('/ls.html')
+
+@app.route('/ls-program', methods=['GET'])
+def ls_program():
+    run_process(['ls'],None, True)
+    return "done", 200
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    global socketio
+    socketio = SocketIO(app, async_mode="threading")
+    socketio.run(app, debug=True, use_reloader=False)
+    # app.run()
